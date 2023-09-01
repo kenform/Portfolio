a12af2bf0d30858fe2661b3d985ace4ea73a2719
Kenform <kenform@bk.ru>
2023-09-01 22:50:45 +0700

    Projects is done... need replase data in every project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import './styles/style.scss';
+import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { darkTheme, lightTheme } from './utils/Themes.js';
 import Header from '../src/components/header/Header';
 import Footer from '../src/components/footer/Footer';
 import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Project from './pages/Project';
 import Contacts from './pages/Contacts';
+import Projects from './components/Projects';
+import ProjectDetails from './components/ProjectDetails';
 
 import ScrollToTop from './utils/scrollToTop';
 
 function App() {
+	const [darkMode, setDarkMode] = useState(true);
+	const [openModal, setOpenModal] = useState({ state: false, project: null });
+	console.log(darkMode);
 	return (
-		<div className='wrapper'>
-			<Router>
-				<ScrollToTop />
-				<Header />
-				<Routes>
-					{/* Маршрут */}
+		<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+			<div className='wrapper'>
+				<Router>
+					<ScrollToTop />
+					<Header />
+					<Routes>
+						{/* Маршрут */}
 
-					<Route path='/' element={<Home />} />
-					<Route path='/projects' element={<Projects />} />
-					<Route path='/project/:id' element={<Project />} />
-					<Route path='/contacts' element={<Contacts />} />
-				</Routes>
-				<Footer />
-			</Router>
-		</div>
+						<Route path='/' element={<Home />} />
+
+						<Route
+							path='/projects'
+							element={<Projects openModal={openModal} setOpenModal={setOpenModal} />}
+						/>
+
+						<Route path='/contacts' element={<Contacts />} />
+					</Routes>
+					<Footer />
+					{openModal.state && <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />}
+				</Router>
+			</div>
+		</ThemeProvider>
 	);
 }
 
